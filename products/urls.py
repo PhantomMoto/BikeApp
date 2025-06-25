a53a38221d50a9c737c70eda918fceac9d4d1ffa
PhantomMoto <singhenterprisesmumbai2024@gmail.com>
2025-06-25 23:13:45 +0530

    media error fixed again 3.0

--- products/urls.py
+++ products/urls.py
@@ -51,8 +51,22 @@ urlpatterns = [
     path('contact/', views.contact_view, name='contact'),
 ]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
-import os
-if settings.DEBUG or os.environ.get('RENDER') == 'true':
+from django.conf import settings
+from django.conf.urls.static import static
+from django.urls import re_path
+from django.views.static import serve
+
+
+
+if not settings.DEBUG:
+    urlpatterns += [
+        re_path(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}),
+    ]
+else:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
     
-    # urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    
+    
+    

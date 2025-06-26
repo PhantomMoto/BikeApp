cc07a531b8456cd0d94b417b1370cd7ac357a519
PhantomMoto <singhenterprisesmumbai2024@gmail.com>
2025-06-26 11:22:06 +0530

    media serving 4

--- products/admin.py
+++ products/admin.py
@@ -1,11 +1,7 @@
 from django.contrib import admin
 from django import forms
-from .models import BikeBrand, BikeModel, Category, Accessory, Blog, YouTubeVideo
+from .models import BikeBrand, BikeModel, Category, Accessory, Blog, YouTubeVideo, Message
 
-
-from django.contrib import admin
-from django import forms
-from .models import Accessory
 
 class AccessoryAdminForm(forms.ModelForm):
     class Meta:
@@ -17,15 +13,20 @@ class AccessoryAdminForm(forms.ModelForm):
         print("Uploaded Image URL:", instance.image.url)
         return instance
 
+
 class AccessoryAdmin(admin.ModelAdmin):
     form = AccessoryAdminForm
+    list_display = ['name', 'price', 'is_universal']
+    filter_horizontal = ['bike_models']
 
-admin.site.register(Accessory, AccessoryAdmin)
+    class Media:
+        js = ('js/accessory_admin.js',)  # agar koi custom JS laga rahe ho
 
 
 @admin.register(Category)
 class CategoryAdmin(admin.ModelAdmin):
     list_display = ['name', 'image']
+
 
 @admin.register(Blog)
 class BlogAdmin(admin.ModelAdmin):
@@ -34,17 +35,15 @@ class BlogAdmin(admin.ModelAdmin):
     search_fields = ("title", "content")
     date_hierarchy = "published_at"
 
+
 @admin.register(YouTubeVideo)
 class YouTubeVideoAdmin(admin.ModelAdmin):
     list_display = ("title", "video_url")
     search_fields = ("title",)
 
+
+# Directly register these without custom admin
 admin.site.register(BikeBrand)
 admin.site.register(BikeModel)
-admin.site.register(Accessory, AccessoryAdmin)  # ✅ FIXED
-
-
-from django.contrib import admin
-from .models import Message
-
+admin.site.register(Accessory, AccessoryAdmin)
 admin.site.register(Message)

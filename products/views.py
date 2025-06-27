ffb015b41bb89bd73b35821e24cc2b262a62e25f
PhantomMoto <singhenterprisesmumbai2024@gmail.com>
2025-06-27 22:33:41 +0530

    delhivery 4

--- products/views.py
+++ products/views.py
@@ -563,7 +563,7 @@ def create_delhivery_order(data):
     api_body = {
         "shipments": [shipment],
         "pickup_location": {
-            "name": "PMOMUMBAIWH"  # Replace with pickup name, NOT CODE
+            "name": "PHANTOM_MOTO"  # Replace with pickup name, NOT CODE
         }
     }
 

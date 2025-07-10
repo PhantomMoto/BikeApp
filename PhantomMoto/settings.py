fb8c449dd9bfa28972dbc61359cf47b55899a8dd
PhantomMoto <singhenterprisesmumbai2024@gmail.com>
2025-07-10 14:07:21 +0530

    abcd

--- PhantomMoto/settings.py
+++ PhantomMoto/settings.py
@@ -3,7 +3,6 @@ import os
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
 SECRET_KEY = 'django-insecure-*p3gkb&fy!1k(51q+hvnl%t#o=5pvf2z9keqnjg(x4jh0qcg3y'
 # DEBUG = os.environ.get('DEBUG')
 DEBUG = True  # Set to False in production
@@ -88,16 +87,13 @@ USE_TZ = True
 # STATIC FILES
 STATIC_URL = '/static/'
 STATIC_ROOT = BASE_DIR / 'staticfiles'
+
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
-import os
-BASE_DIR = Path(__file__).resolve().parent.parent
 
 
-import os
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_ROOT = BASE_DIR / 'media'
 
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'

41cb266b717a8a160cd100ddedeb9dc2873e86f6
IshanKashyap123 <ishan47.kashyap@gmail.com>
2025-11-17 21:36:46 +0530

    a

--- products/views.py
+++ products/views.py
@@ -1003,7 +1003,7 @@ def shipping_form(request):
                     'quantity': qty,
                 })
                 
-        request.session['final_amount'] = float(total)*0.9 if mode == "COD" else float(total)
+        request.session['final_amount'] = float(total) if mode == "Pre-paid" else float(total)*0.9
         total_width = 0
         total_height = 0
         total_weight = 0
